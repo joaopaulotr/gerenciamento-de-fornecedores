5dd4f99344c185b0f79ad356b2bf0ecf5104958a
João Paulo <joaopaulotraguetta14@gmail.com>
2026-03-18 19:45:43 -0300

    copiado de gerenciamento-de-livros

---
+++ demo/src/main/java/com/_exercicios/demo/DemoApplication.java
@@ -0,0 +1,13 @@
+package com._exercicios.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
